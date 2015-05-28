8349188fc6ed969aa656c9ada9746a848942c39e
ThePersister <Ahm_Brouwers@hotmail.com>
2015-05-28 21:07:00 +0200

    Spheres spawn and move across table :+1:

---
+++ Assets/_Main/Scripts/MoveForward.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveForward : MonoBehaviour {
+
+    public float Speed = 5f;
+    private Transform _transform;
+
+	void Start () {
+        _transform = transform;
+	}
+
+	void Update () {
+        _transform.position += transform.forward * Time.deltaTime * Speed;
+	}
+}
--- Assets/_Main/Scripts/SpawnModels.cs
+++ Assets/_Main/Scripts/SpawnModels.cs
@@ -22,6 +22,6 @@ public class SpawnModels : MonoBehaviour {
 
     private GameObject GetRandomModel()
     {
-        return Models[Mathf.RoundToInt(Random.value * Models.Count)];
+        return Models[Mathf.RoundToInt(Random.value * (Models.Count -1))];
     }
 }
