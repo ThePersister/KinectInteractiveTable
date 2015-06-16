23def2fe09df6508604dfb00ca1dbae0648d1eb6
ThePersister <Ahm_Brouwers@hotmail.com>
2015-06-16 15:37:22 +0200

    Photon Import And NetworkManager

---
+++ Assets/_Main/Scripts/NetworkManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PhotonView))]
+public class NetworkManager : Singleton<NetworkManager> {
+
+    private PhotonView pView;
+
+	void Start () {
+        pView = GetComponent<PhotonView>();
+	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+            SM_PlayGame();
+
+    }
+
+    public void SM_PlayGame()
+    {
+        Debug.Log("Sent Message");
+        pView.RPC("RM_PlayGame", PhotonTargets.OthersBuffered);
+    }
+
+    [RPC]
+    public void RM_PlayGame()
+    {
+        Debug.Log("Received Message");
+        //GameManager.Instance.Start();
+    }
+}
